2d90036247a3e9b8a21de9b0f3b9c3d7864223d9
YakoYakov <yako.p.yakov@gmail.com>
2020-06-24 18:34:28 +0300

    Injected Auth service in nav component

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
 
-  baseUrl = 'http://localhost:5000/api/auth';
+  baseUrl = 'http://localhost:5000/api/auth/';
 
   constructor(private http: HttpClient) { }
 
