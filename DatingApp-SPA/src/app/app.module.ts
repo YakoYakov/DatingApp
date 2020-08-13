46942fb756803a51f5512db0887eb44dfc4be360
YakoYakov <yako.p.yakov@gmail.com>
2020-08-13 17:46:15 +0300

    Adding the send like functionality to the SPA, Creating the list component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolve';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { ListResolver } from './_resolvers/list-resolver';
 
 
 export function tokenGetter() {
@@ -89,6 +90,7 @@ export function tokenGetter() {
       MemberDetailResolver,
       MemberListResolver,
       MemberEditResolver,
+      ListResolver,
       PreventUnsavedChanges
    ],
    bootstrap: [
--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { Pagination, PaginatedResult } from '../_models/pagination';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  users: User[];
+  pagination: Pagination;
+  likesParams: string;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private userService: UserService,
+              private alertify: AlertifyService,
+              private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.router.data.subscribe(data => {
+      this.users = data.users.result;
+      this.pagination = data.users.pagination;
+    });
+    this.likesParams = 'Likers';
+  }
+
+  loadUsers() {
+    this.userService
+      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, null, this.likesParams)
+      .subscribe(
+        (res: PaginatedResult<User[]>) => {
+          this.users = res.result;
+          this.pagination = res.pagination;
+        },
+        error => {
+          this.alertify.error(error);
+        }
+      );
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
   }
 
 }
